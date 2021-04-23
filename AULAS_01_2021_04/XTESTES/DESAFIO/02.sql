29031fbbf44a64329975fd298477fb1e6c1befd0
Iuri Albuquerque Reis <iareis@gmail.com>
2021-04-23 15:42:11 -0300

    Desafio 2 OK as 15:42@23/04/2021

--- AULAS_01_2021_04/XTESTES/DESAFIO/02.sql
+++ AULAS_01_2021_04/XTESTES/DESAFIO/02.sql
@@ -1,7 +1,7 @@
 ====================================================================================================
 =                                                                                                  =
-=    Criar uma função que deverá receber um número inteiro e retornar se o mesmo é primo ou não    =
-=    (Lembrete: Números primos são divisíveis somente por eles mesmos e por um).                   = 
+=    Criar uma funcao que devera receber um numero inteiro e retornar se o mesmo eh primo ou não    =
+=    (Lembrete: Numeros primos sao divisiveis somente por eles mesmos e por um).                   = 
 =                                                                                                  =
 ====================================================================================================
 
@@ -40,7 +40,7 @@ END;
 
 
 =======================
-===   SITUACAO 02   === 
+===   SITUACAO 02   === ERRO
 =======================
 
 CREATE OR REPLACE FUNCTION CONSULTA_PRIMO (V_RESPOSTA NUMBER) RETURN VARCHAR2
@@ -70,32 +70,32 @@ END;
 
 
 =======================
-===   SITUACAO 03   === 
+===   SITUACAO 03   ===  OK
 =======================
 
-CREATE OR REPLACE FUNCTION CONSULTA_PRIMO (V_RESPOSTA NUMBER) RETURN VARCHAR2
-IS  V_DIVIDENDO NUMBER(6);
-    V_DIVISOR NUMBER(6);
-    V_RESPOSTA VARCHAR2(20);
+2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 
+83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 
+173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263,
+ 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
+
+CREATE OR REPLACE FUNCTION CONSULTA_PRIMO (V_NUMERO NUMBER) RETURN VARCHAR2
+IS  V_PRIMO VARCHAR2(30) := 'Eh um numero PRIMO!';
+    V_NPRIMO VARCHAR2(30) := 'NAO Eh um numero PRIMO!';
 BEGIN 
-    FOR V_DIVISOR IN 2..V_DIVIDENDO*2
+    FOR V_DIVISOR IN 2..V_NUMERO-1
     LOOP 
-        IF MOD(V_DIVIDENDO,V_DIVISOR) = 0 AND V_DIVIDENDO != V_DIVISOR THEN
-            DBMS_OUTPUT.PUT_LINE(V_DIVIDENDO||' NÃO é um número PRIMO!');
-        ELSE MOD(V_DIVIDENDO,V_DIVISOR) = 0 AND V_DIVIDENDO = V_DIVISOR THEN
-            DBMS_OUTPUT.PUT_LINE(V_DIVIDENDO||' É um número PRIMO!');
+        IF MOD(V_NUMERO,V_DIVISOR) = 0 THEN
+            RETURN (V_NPRIMO);
         END IF;        
     END LOOP;
-    RETURN V_RESPOSTA;
+    RETURN (V_PRIMO);
 END;
+/*
+Function CONSULTA_PRIMO compilado
+*/
 
 
-DECLARE
-    V_NUMBER NUMBER := &NUMERO;
-BEGIN 
-    V_RESPOSTA := CONSULTA_PRIMO(V_NUMBER);
-    DBMS_OUTPUT.PUT_LINE(V_RESPOSTA);
-END;
+SELECT CONSULTA_PRIMO(&DIGA_UM_NUMERO) FROM DUAL;
 
 
 

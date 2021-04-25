f3df201e66c964af714f0e64e40a101fcd0561eb
Iuri Albuquerque Reis <iareis@gmail.com>
2021-04-25 16:25:34 -0300

    Desafio 08 OK as 16:25@25/04/2021

---
+++ AULAS_01_2021_04/XTESTES/DESAFIO/08.sql
@@ -0,0 +1,59 @@
+===============================================================================
+===   Criar uma procedure que devera receber o codigo de um cliente         ===
+===   e a partir deste dado imprimir o seu nome, e seu e‐mail.              ===
+===   Os dados deverão ser obtidos a partir de uma tabela chamada CLIENTE   ===
+===   com as seguintes colunas (COD_CLI,NOME_CLI,EMAIL_CLI)                 ===
+===============================================================================
+
+/* Criar a tabela cliente */
+CREATE TABLE CLIENTE (
+    COD_CLI NUMBER(4) PRIMARY KEY,
+    NOME_CLI VARCHAR2(30),
+    EMAIL_CLI VARCHAR2(30)
+);
+/*
+Table CLIENTE criado.
+*/
+
+/* inserir o dado*/
+INSERT INTO CLIENTE VALUES (10,'BEATRIZ BERNARDES','bb@dominio.com.br');
+/*
+1 linha inserido.
+*/
+
+===============================================================================
+
+CREATE OR REPLACE PROCEDURE RECEBE_CLIENTE (COD_CLIENTE NUMBER)
+IS 
+    V_CLIENTE CLIENTE%ROWTYPE;
+BEGIN 
+    SELECT *
+    INTO V_CLIENTE 
+    FROM CLIENTE
+    WHERE COD_CLI = COD_CLIENTE;
+    ---
+    DBMS_OUTPUT.PUT_LINE('NOME  : '||V_CLIENTE.NOME_CLI);
+    DBMS_OUTPUT.PUT_LINE('E-MAIL: '||V_CLIENTE.EMAIL_CLI);
+END;
+/*
+Procedure RECEBE_CLIENTE compilado
+*/
+
+DECLARE
+BEGIN 
+    RECEBE_CLIENTE(&COD_CLIENTE);
+    DBMS_OUTPUT.PUT_LINE('NOME  : '||V_CLIENTE.NOME_CLI);
+    DBMS_OUTPUT.PUT_LINE('E-MAIL: '||V_CLIENTE.EMAIL_CLI);
+END;
+
+
+
+
+
+
+
+
+
+
+
+

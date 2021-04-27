14ee9175cde069a9d1a6751ca570b545b26fe29a
Iuri Albuquerque Reis <iareis@gmail.com>
2021-04-27 09:21:46 -0300

    criado enunciado para Desafio 10

--- AULAS_01_2021_04/XTESTES/DESAFIO/10.sql
+++ AULAS_01_2021_04/XTESTES/DESAFIO/10.sql
@@ -3,6 +3,30 @@
 ===   Imposto sobre Produtos Industrializados   ===
 ===================================================
 
+Exemplo de Cálculo do IPI
+O IPI é calculado tendo como base a alíquota descrita na TIPI.
+Para indústrias, o Imposto sobre Produtos Industrializados é calculado 
+em relação ao valor da nota fiscal da mercadoria despachada. 
+A NF pode eventualmente incluir valores sobre o frete e despesas como juros, 
+taxas, entre outras.
+
+Valor do IPI = Base de cálculo * (Alíquota / 100)
+
+Sendo que:
+
+Base de cálculo = (Valor do produto + Frete + Seguro + Outras Despesas Acessórias)
+
+Observando a fórmula, você verá que descontos não são considerados na 
+base de cálculo do IPI. Isso significa que se forem dados descontos 
+quando o produto sofre tributação do IPI a empresa pagará tributos 
+sobre a parte que sofreu desconto. Por isso, descontos nesse caso 
+não são uma boa ideia. O melhor a fazer, nesta situação, é reduzir 
+o preço unitário do item.
+
+Importante também destacar que o valor do IPI é somado ao valor total 
+do produto na linha da NF-e.
+
+=============================================================================
 
 
 
@@ -51,42 +75,3 @@
 
 
 
-
-
-
-DECLARE
-
-  PROCEDURE CALCULA_IPI(CODPRODUTO NUMBER ) IS
-    
-  VPRO PRODUTO_TESTE2.VALOR%TYPE;
-  VIPI ALIQUOTA.IPI%TYPE;
-  TIPI NUMBER(6,2);
-    
-  BEGIN
-
- --ENCONTRAR O VALOR DA TABELA PRODUTO
-     SELECT P.VALOR
-      INTO VPRO
-     FROM PRODUTO_TESTE2 P
-      WHERE P.CODIGO = CODPRODUTO;
-  
- --ENCONTRAR O VALOR DA TABELA IPI
-    SELECT A.IPI 
-      INTO VIPI 
-    FROM PRODUTO_TESTE2 P 
-      INNER JOIN ALIQUOTA A
-      ON P.CATEGORIA = A.COD_CAT 
-    WHERE P.CODIGO = CODPRODUTO;
-
-  --CALCULO DO VALOR DO IP
-  TIPI := VPRO * (VIPI/100);
-  
-  --EXIBIR NA TELA
-  DBMS_OUTPUT.PUT_LINE('TOTAL IPI: ' || TIPI);
-  END;
-
-BEGIN
-
-  CALCULA_IPI(1003);
-
-END;

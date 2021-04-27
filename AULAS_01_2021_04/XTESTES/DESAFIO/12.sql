3f0aac0209e13ad876a281e32490802e556a92fe
Iuri Albuquerque Reis <iareis@gmail.com>
2021-04-27 09:26:22 -0300

    criado enunciado para Desafio 12

---
+++ AULAS_01_2021_04/XTESTES/DESAFIO/12.sql
@@ -0,0 +1,5 @@
+===============================================
+===   Criar uma funcao que dado um salario  ===
+===   retorne o numero de salarios minimos  === 
+===   que a pessoa ganha.                   ===
+===============================================

62e01a6f9993acc646af8b60857cd13b823a37a8
Iuri Albuquerque Reis <iareis@gmail.com>
2021-04-27 10:25:12 -0300

    Desafio 12 OK as 10:24@27/04/2021

--- AULAS_01_2021_04/XTESTES/DESAFIO/12.sql
+++ AULAS_01_2021_04/XTESTES/DESAFIO/12.sql
@@ -3,3 +3,23 @@
 ===   retorne o numero de salarios minimos  === 
 ===   que a pessoa ganha.                   ===
 ===============================================
+
+/*
+Em 1º de janeiro de 2021, o salário mínimo subiu de R$ 1.045 para 1.100.
+*/
+
+
+CREATE OR REPLACE FUNCTION SALARIO_MINIMO(
+    SALARIO NUMBER
+) RETURN NUMBER
+IS 
+NUM_SAL_MINIMOS NUMBER(8,2);
+BEGIN 
+    NUM_SAL_MINIMOS := SALARIO/1100;
+    RETURN NUM_SAL_MINIMOS;
+END;
+
+
+
+
+SELECT SALARIO_MINIMO(&QUAL_SALARIO) AS QTDE_SAL_MIN FROM DUAL;

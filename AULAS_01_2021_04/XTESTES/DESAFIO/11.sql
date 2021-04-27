14948c669c615af2888e6d8c2951f859327a0511
Iuri Albuquerque Reis <iareis@gmail.com>
2021-04-27 09:24:02 -0300

    criado enunciado para Desafio 11

---
+++ AULAS_01_2021_04/XTESTES/DESAFIO/11.sql
@@ -0,0 +1,4 @@
+=========================================================== 
+===   Criar uma funcao que dado o numero de minutos     ===
+===   retorne o numero de horas correspondente.         ===
+=========================================================== 

fe8612c57e6d0b52100088bb5446669bf8f4d6dc
Iuri Albuquerque Reis <iareis@gmail.com>
2021-04-30 15:56:03 -0300

    Desafio 16 OK as 15:55@30/04/2021

--- AULAS_01_2021_04/XTESTES/DESAFIO/16.sql
+++ AULAS_01_2021_04/XTESTES/DESAFIO/16.sql
@@ -1,13 +1,23 @@
 =========================================================
-===   Criar uma função de converter Real em Dólar.    ===
+===   Criar uma funcao de converter Real em Dolar.    ===
 =========================================================
 
+CREATE OR REPLACE FUNCTION F_REAL_DOLAR (MOEDA_BRASIL NUMBER) RETURN NUMBER
+IS 
+CONV_REAL_DOLAR NUMBER(8,2);
+BEGIN 
+    CONV_REAL_DOLAR := MOEDA_BRASIL / 5.47;
+    DBMS_OUTPUT.PUT_LINE('VALOR EM MOEDA NACIONAL: '||TO_CHAR(MOEDA_BRASIL,'L999G999D99'));
+    DBMS_OUTPUT.PUT_LINE('COTACAO DO DOLAR:        '||5.47);
+    DBMS_OUTPUT.PUT_LINE('------------------------------------------------------');
+    DBMS_OUTPUT.PUT_LINE('VALOR EM DOLARES:        '||TO_CHAR(CONV_REAL_DOLAR,'L999G999D99MI','NLS_NUMERIC_CHARACTERS = '',.''NLS_CURRENCY = ''US$'' '));
+    RETURN CONV_REAL_DOLAR;
+END;
 
 
 
 
-
-
+SELECT F_REAL_DOLAR(&MOEDA_BRASIL) FROM DUAL;
 
 
 
